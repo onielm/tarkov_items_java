fc9e99d54ce3fab6fe0e5ea447a3a805c3b138df
onielm <oniel.mendez2@upr.edu>
2021-05-24 13:29:26 -0400

    Vendors and Items are now listed
    Next is loading and saving items status

--- src/tarkov_items.java
+++ src/tarkov_items.java
@@ -4,12 +4,27 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import javax.imageio.ImageIO;
+import javax.json.JsonReader;
 import javax.swing.*;
+
+import org.apache.commons.io.FileUtils;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Map;
 
 public class tarkov_items {
 
@@ -23,180 +38,191 @@ public class tarkov_items {
 	final JScrollPane scroll = new JScrollPane(back_panel);
 
 
-    public tarkov_items() throws IOException {
-			Integer cardWidth = 350;
-			
+    public tarkov_items() throws IOException, ParseException {
+		Integer cardWidth = 350;
+		Integer most_items = -1;
+		Integer card_and_space_size_y = -1;
+		Integer item_card_position_y = 70;
+
 		back_panel.setBackground(new Color(0,0,0));
 		back_panel.setLayout(null);
 		back_panel.setPreferredSize(new Dimension(8*(cardWidth+40), 1000));
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.setSize(2500, 1000);
 		frame.setTitle("Tarkov in Raid Items");
-//		frame.setLocationRelativeTo(null);
 		frame.setLayout(null);
 		frame.setLayout(new BorderLayout());
 		frame.add(scroll, BorderLayout.CENTER);
 		frame.setSize(1000, 600);
 		
+		
+		String file = "images/data.json";
+
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(file);
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jo = (JSONObject)jsonParser.parse(new InputStreamReader(inputStream, "UTF-8"));
+
         String[] traders = {"Prapor", "Therapist", "Fence", "Skier", 
         					"Peacekeeper","Mechanic","Ragman", "Jaeger"};
         Integer panel_x = 50;
         Integer panel_y = 50;
-        for (String trader : traders) 
+		for (String trader : traders) 
         { 
-        	// Buttons
-//  			JButton btnNotFound = new JButton("Not Found");
-        	
-            JButton btnNotFound = new JButton("Not Found");
-            JButton btnInStash = new JButton("In Stash");
-            JButton btnTurnedIn = new JButton("Turned In");
 
-  			btnNotFound.setBackground(notFoundColor);
-  			btnNotFound.setBounds(5, 5, 100, 30);
-  			btnNotFound.addActionListener(new ActionListener() { 
-	  			public void actionPerformed(ActionEvent e) { 
-	  				SetItemStatus(btnNotFound); }
-	  			public void SetItemStatus(Container container) {
-  		    		container.getParent().setBackground(notFoundColor);
-	  				return;
-	  			}
-  			} );
-  			btnNotFound.setLayout(null);
-  			
-//  			JButton btnInStash = new JButton("In Stash");
-  			btnInStash.setBackground(inStashColor);
-  			btnInStash.setBounds(5, 35, 100, 30);
-  			btnInStash.addActionListener(new ActionListener() { 
-	  			public void actionPerformed(ActionEvent e) { 
-  					SetItemStatus(btnInStash); }
-	  			public void SetItemStatus(Container container) {
-  		    		container.getParent().setBackground(inStashColor);
-	  				return;
-	  			}
-  			} );
-  			btnInStash.setLayout(null);
+        
 
-//  			JButton btnTurnedIn = new JButton("TurnedIn");
-  			btnTurnedIn.setBackground(turnedInColor);
-  			btnTurnedIn.setBounds(5, 65, 100, 30);
-  			btnTurnedIn.addActionListener(new ActionListener() { 
-	  			public void actionPerformed(ActionEvent e) { 
-	  				SetItemStatus(btnTurnedIn); }
-	  			public void SetItemStatus(Container container) {
-  		    		container.getParent().setBackground(turnedInColor);
-	  				return;
-	  			}
-  			} );
-  			btnTurnedIn.setLayout(null);
-  			
-  			//vendor image
-  			
-
-  			
-  			
-  			
-//  			System.out.println("Working Directory = " + System.getProperty("user.dir"));
-//  			BufferedImage img = ImageIO.read(new File("src/images/fence/Fence_Portrait.webp"));
-//  			JLabel vendorImg = new JLabel(new ImageIcon(img));
-//  			btnTurnedIn.setBounds(5, 65, 50, 50);
-//  			btnTurnedIn.setLayout(null);
-
-  			
-  			//item image
-  			
-  			
-  			// item card panel
-  			JPanel item_card = new JPanel();
-  			item_card.setBackground(notFoundColor);
-  			item_card.setBounds(5, 70, cardWidth, 100);
-  			item_card.setLayout(null);
-  			item_card.setPreferredSize(new Dimension(800, 600));
-  			
-  			Integer itemImageSize = item_card.getHeight() - 10;
-  			String item_image = "/images/fence/Pestily_plague_mask.jpg";
-  			ImageIcon item_icon = LoadImageIcon(item_image,itemImageSize);
-  			JLabel itemImg = new JLabel();
-  			itemImg.setIcon(item_icon);
-  			itemImg.setBounds(btnTurnedIn.getWidth()+10,5, itemImageSize, itemImageSize);
-  			itemImg.setLayout(null);
-  			itemImg.setVisible(true);
-
-  			Integer itemAmount = 1;
-  			JLabel itemTextBox = new JLabel("<html>"+ itemAmount.toString() + "x<br>Pestily plague mask" +"</html>");
-  			itemTextBox.setBounds(15+btnTurnedIn.getWidth()+itemImg.getWidth(),
-  									5, 
-  									cardWidth - 20 - btnTurnedIn.getWidth() - itemImg.getWidth(),
-  									2*item_card.getHeight()/3-5);
-  			itemTextBox.setFont(new Font("Arial", Font.PLAIN, 12));
-  			itemTextBox.setForeground(new Color(255,255,255));
-  			itemTextBox.setOpaque(true);
-  			itemTextBox.setBackground(new Color(81,81,81,155));
-  			itemTextBox.setLayout(null);
-  			itemTextBox.setVisible(true);
-  			
-  			//craft label
-  			Integer craftIconSize = item_card.getHeight()/3-9;
-  			String craft_image = "/images/tools.png";
-  			ImageIcon craft_icon = LoadImageIcon(craft_image,craftIconSize);
-  			JLabel craftImg = new JLabel();
-  			craftImg.setIcon(craft_icon);
-  			craftImg.setBounds(15+btnTurnedIn.getWidth()+itemImg.getWidth(),
-  							11 + itemImg.getWidth()/3*2, 
-  								craftIconSize, 
-  								craftIconSize);
-  			craftImg.setLayout(null);
-  			craftImg.setBackground(new Color(81,81,81,155));
-  			craftImg.setVisible(true);
-  			//craft text
-  			JLabel craftableLabel = new JLabel("<html><b><span style='text-align: center;'>"
-  											+ "CRAFTABLE</span></b></html>");
-  			craftableLabel.setBounds(15+btnTurnedIn.getWidth()+itemImg.getWidth()+craftIconSize,
-  								11 + itemImg.getWidth()/3*2, 
-  								30+(cardWidth - 20 - btnTurnedIn.getWidth() - itemImg.getWidth())/3,
-  								item_card.getHeight()/3-9);
-  			craftableLabel.setFont(new Font("Arial", Font.PLAIN, 12));
-			craftableLabel.setForeground(new Color(255,255,255));
-			craftableLabel.setOpaque(true);
-  			craftableLabel.setBackground(new Color(81,81,81,155));
-  			craftableLabel.setLayout(null);
-			craftableLabel.setVisible(true);
-  			
-  		
-  			//kappa label
-//  			JLabel kappaLabel = new JLabel("<html><b><span style='text-decoration: line-through;text-align: center;'>"
-//  								+ "KAPPA</span></b></html>");
-//  			kappaLabel.setBounds(20+btnTurnedIn.getWidth()+itemImg.getWidth()+craftableLabel.getWidth(),
-//  								11 + itemImg.getWidth()/3*2, 
-//  								(cardWidth - 20 - btnTurnedIn.getWidth() - itemImg.getWidth())/3,
-//  								item_card.getHeight()/3-9);
-//  			kappaLabel.setFont(new Font("Arial", Font.PLAIN, 12));
-//  			kappaLabel.setForeground(new Color(255,255,255));
-//  			kappaLabel.setOpaque(true);
-//  			kappaLabel.setBackground(new Color(81,81,81,155));
-//  			kappaLabel.setLayout(null);
-//  			kappaLabel.setVisible(true);
-  			Integer kappaIconSize = item_card.getHeight()/3-9;
-  			String kappa_image = "/images/not_kappa.png";
-  			ImageIcon kappa_icon = LoadImageIcon(kappa_image,kappaIconSize);
-  			JLabel kappaImg = new JLabel();
-  			kappaImg.setIcon(kappa_icon);
-  			kappaImg.setBounds(cardWidth-5 - kappaIconSize,
-  							11 + itemImg.getWidth()/3*2, 
-  								kappaIconSize, 
-  								kappaIconSize);
-  			kappaImg.setLayout(null);
-  			kappaImg.setVisible(true);
-  			// JPanel
   			JPanel fence_panel = new JPanel();
+  			Integer item_card_size_y = 100;
+  			Integer item_card_space_y_after= 10;
+  			Integer item_card_start_position_y = item_card_position_y;
+  			card_and_space_size_y = item_card_size_y+item_card_space_y_after;
+  			// while loop to create each item card for curent vendor panel
+	        Map items = ((Map)jo.get(trader));
+
+	        //System.out.println(items);
+	        //System.out.println(items.size());
+	        Iterator<Map.Entry> itr1 = items.entrySet().iterator();
+	        while (itr1.hasNext()) {
+	            Map.Entry pair = itr1.next();
+	            //System.out.println(pair.getKey() + " : " + pair.getValue());	            
+	            JSONObject itemInfo = (JSONObject) pair.getValue();
+	            
+	            String item_name = (String) itemInfo.get("name");
+	            String item_quantity = (String) itemInfo.get("quantity");
+	            String item_kappa = (String) itemInfo.get("kappa");
+	            String item_level = (String) itemInfo.get("level");
+	            String item_crafting = (String) itemInfo.get("crafting");
+	            //String item_status = (String) itemInfo.get("status"); //quizas para guardar data. me parece que no.
+	            String item_image_name= (String) itemInfo.get("image");
+	            //
+	        	// Buttons
+	            JButton btnNotFound = new JButton("Not Found");
+	            JButton btnInStash = new JButton("In Stash");
+	            JButton btnTurnedIn = new JButton("Turned In");
+
+	  			btnNotFound.setBackground(notFoundColor);
+	  			btnNotFound.setBounds(5, 5, 100, 30);
+	  			btnNotFound.addActionListener(new ActionListener() { 
+		  			public void actionPerformed(ActionEvent e) { 
+		  				SetItemStatus(btnNotFound); }
+		  			public void SetItemStatus(Container container) {
+	  		    		container.getParent().setBackground(notFoundColor);
+		  				return;
+		  			}
+	  			} );
+	  			btnNotFound.setLayout(null);
+	  			
+	  			btnInStash.setBackground(inStashColor);
+	  			btnInStash.setBounds(5, 35, 100, 30);
+	  			btnInStash.addActionListener(new ActionListener() { 
+		  			public void actionPerformed(ActionEvent e) { 
+	  					SetItemStatus(btnInStash); }
+		  			public void SetItemStatus(Container container) {
+	  		    		container.getParent().setBackground(inStashColor);
+		  				return;
+		  			}
+	  			} );
+	  			btnInStash.setLayout(null);
+
+	  			btnTurnedIn.setBackground(turnedInColor);
+	  			btnTurnedIn.setBounds(5, 65, 100, 30);
+	  			btnTurnedIn.addActionListener(new ActionListener() { 
+		  			public void actionPerformed(ActionEvent e) { 
+		  				SetItemStatus(btnTurnedIn); }
+		  			public void SetItemStatus(Container container) {
+	  		    		container.getParent().setBackground(turnedInColor);
+		  				return;
+		  			}
+	  			} );
+	  			btnTurnedIn.setLayout(null);
+	  			
+		        
+	  			
+	  			// item card panel
+	  			JPanel item_card = new JPanel();
+	  			item_card.setBackground(notFoundColor);
+	  			item_card.setBounds(5, item_card_start_position_y, cardWidth, item_card_size_y);
+	  			item_card.setLayout(null);
+	  			item_card.setPreferredSize(new Dimension(800, 600));
+	  			
+	  			Integer itemImageSize = item_card.getHeight() - 10;
+	  			String item_image = "images/"+trader+"/"+item_image_name;
+		        System.out.println(item_image);
+
+	  			ImageIcon item_icon = LoadImageIcon(item_image,itemImageSize);
+	  			JLabel itemImg = new JLabel();
+	  			itemImg.setIcon(item_icon);
+	  			itemImg.setBounds(btnTurnedIn.getWidth()+10,5, itemImageSize, itemImageSize);
+	  			itemImg.setLayout(null);
+	  			itemImg.setVisible(true);
+
+	  			JLabel itemTextBox = new JLabel("<html>"+ item_quantity + "x<br>" + item_name +"</html>");
+	  			itemTextBox.setBounds(15+btnTurnedIn.getWidth()+itemImg.getWidth(),
+	  									5, 
+	  									cardWidth - 20 - btnTurnedIn.getWidth() - itemImg.getWidth(),
+	  									2*item_card.getHeight()/3-5);
+	  			itemTextBox.setFont(new Font("Arial", Font.PLAIN, 12));
+	  			itemTextBox.setForeground(new Color(255,255,255));
+	  			itemTextBox.setOpaque(true);
+	  			itemTextBox.setBackground(new Color(81,81,81,155));
+	  			itemTextBox.setLayout(null);
+	  			itemTextBox.setVisible(true);
+	  			
+	  			//craft label
+	  			Integer craftIconSize = item_card.getHeight()/3-9;
+	  			JLabel craftImg = new JLabel();
+//	  			String craft_image = "/tools.png";
+//	  			ImageIcon craft_icon = LoadImageIcon(craft_image,craftIconSize);
+//	  			craftImg.setIcon(craft_icon);
+//	  			craftImg.setBounds(15+btnTurnedIn.getWidth()+itemImg.getWidth(),
+//	  							11 + itemImg.getWidth()/3*2, 
+//	  								craftIconSize, 
+//	  								craftIconSize);
+//	  			craftImg.setLayout(null);
+//	  			craftImg.setBackground(new Color(81,81,81,155));
+//	  			craftImg.setVisible(true);
+	  			
+	  		
+	  			Integer kappaIconSize = item_card.getHeight()/3-9;
+	  			JLabel kappaImg = new JLabel();
+//	  			String kappa_image = "/not_kappa.png";
+//	  			ImageIcon kappa_icon = LoadImageIcon(kappa_image,kappaIconSize);
+//	  			kappaImg.setIcon(kappa_icon);
+//	  			kappaImg.setBounds(cardWidth-5 - kappaIconSize,
+//							11 + itemImg.getWidth()/3*2, 
+//								kappaIconSize, 
+//								kappaIconSize);
+//	  			kappaImg.setLayout(null);
+//	  			kappaImg.setVisible(true);
+	            
+	            //
+	      		item_card.add(btnNotFound);
+	      		item_card.add(btnInStash);
+	      		item_card.add(btnTurnedIn);
+	      		item_card.add(itemImg);
+	      		item_card.add(itemTextBox);
+	      		item_card.add(craftImg);
+	      		item_card.add(kappaImg);
+	      		fence_panel.add(item_card);
+	      		
+	      		item_card_start_position_y = item_card_start_position_y + card_and_space_size_y;
+	        }
+
+  			// JPanel
   			fence_panel.setBackground(new Color(51,51,51));
-  			fence_panel.setBounds(panel_x, panel_y, cardWidth+10, 300);
+  			fence_panel.setBounds(panel_x, panel_y, cardWidth+10, item_card_position_y+(items.size() * (item_card_size_y + item_card_space_y_after)));
   			fence_panel.setLayout(null);
   			fence_panel.setPreferredSize(new Dimension(800, 600));
   			
+  			if ( items.size() > most_items) {
+  				most_items = items.size();
+  			}
+  			
   			//vendor image; goes over panel
   			Integer vendorImageSize = 60;
-  			String vendor_image = "/images/fence/Fence_Portrait.jpg";
+  			String vendor_image = "images/"+trader+"/"+trader+"_Portrait.jpg";
+            //System.out.println(vendor_image);	            
+
   			ImageIcon vendor_icon = LoadImageIcon(vendor_image,vendorImageSize);
   			
   			JLabel vendorImg = new JLabel();
@@ -206,7 +232,7 @@ public class tarkov_items {
   			vendorImg.setLayout(null);
   			vendorImg.setVisible(true);
 
-  			JLabel vendorName = new JLabel("Fence", SwingConstants.CENTER);
+  			JLabel vendorName = new JLabel(trader, SwingConstants.CENTER);
   			vendorName.setBounds(0,0, (fence_panel.getWidth()-vendorImageSize), 100);
   			vendorName.setFont(new Font("Arial", Font.PLAIN, 32));
   			vendorName.setForeground(new Color(255,255,255));
@@ -214,46 +240,21 @@ public class tarkov_items {
   			vendorName.setVisible(true);
 
   			
-  			// 
-
-  			
-
-  			
 
   			
       		//add
-      		item_card.add(btnNotFound);
-      		item_card.add(btnInStash);
-      		item_card.add(btnTurnedIn);
-      		item_card.add(itemImg);
-      		item_card.add(itemTextBox);
-      		item_card.add(craftImg);
-      		item_card.add(kappaImg);
+
       		fence_panel.add(vendorImg);
       		fence_panel.add(vendorName);
-      		fence_panel.add(item_card);
       		back_panel.add(fence_panel);
             panel_x = panel_x + cardWidth + 20;
         }
-        
+		back_panel.setPreferredSize(new Dimension(8*(cardWidth+40), (most_items+1)*card_and_space_size_y+panel_y+item_card_position_y));
 		frame.setVisible(true);
 
     }
-    
-//	public void SetItemStatus(Container container) {
-//    	if(container == btnNotFound) {
-//    		container.getParent().setBackground(notFoundColor);
-//    	}
-//    	else if(container == btnInStash) {
-//    		container.getParent().setBackground(inStashColor);
-//    	}
-//    	else if(container == btnTurnedIn) {
-//    		container.getParent().setBackground(turnedInColor);
-//    	}
-//		return;
-//	}
-	
-	//load an image and resize it
+
+    //load an image and resize it
 	protected ImageIcon LoadImageIcon(String path, Integer newSquareSize) {
         ImageIcon icon = new ImageIcon(this.getClass().getResource(path));
         Image image = icon.getImage(); // transform it 
@@ -261,17 +262,7 @@ public class tarkov_items {
         return new ImageIcon(newimg);  // transform it back
 	}
 	
-//	protected ImageIcon createImageIcon(String path,
-//    			String description) {
-//		java.net.URL imgURL = getClass().getResource(path);
-//		if (imgURL != null) {
-//			return new ImageIcon(imgURL, description);
-//		} else {
-//			System.err.println("Couldn't find file: " + path);
-//			return null;
-//		}
-//	}
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
         new tarkov_items();
     }
 }
